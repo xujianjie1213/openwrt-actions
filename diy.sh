ceba80f2d926283e5c0ff8e7815910faca02823d
xujianjie1213 <35050408+xujianjie1213@users.noreply.github.com>
2020-01-15 10:57:53 +0800

    Add files via upload

---
+++ diy.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#=================================================
+# Description: DIY script
+# Lisence: MIT
+# Author: P3TERX
+# Blog: https://p3terx.com
+#=================================================
+# Modify default IP
+#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.2.254/g' package/base-files/files/bin/config_generate
+git clone https://github.com/tty228/luci-app-serverchan package/luci-app-serverchan
+git clone https://github.com/openwrt-develop/luci-theme-atmaterial package/luci-theme-atmaterial
+git clone https://github.com/rosywrt/luci-theme-rosy package/luci-theme-rosy
+git clone https://github.com/hong0980/luci-app-passwall package/luci-app-passwall
+git clone https://github.com/Leo-Jo-My/luci-app-ssr-plus-Jo package/luci-app-ssr-plus-Jo
