f0fa7c9f0fa8f07e5224b343cc78c698f919d2c1
xujianjie1213 <35050408+xujianjie1213@users.noreply.github.com>
2020-02-02 20:42:18 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -9,6 +9,6 @@
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
 sed -i 's/192.168.1.1/192.168.2.254/g' package/base-files/files/bin/config_generate
 git clone https://github.com/tty228/luci-app-serverchan package/luci-app-serverchan
-git clone https://github.com/Leo-Jo-My/luci-app-ssr-plus-Jo package/luci-app-ssr-plus-Jo
+git clone https://github.com/Leo-Jo-My/luci-app-ssr-plus-Jo package/my/luci-app-ssr-plus-Jo
 git clone https://github.com/openwrt-develop/luci-theme-atmaterial package/luci-theme-atmaterial
 git clone https://github.com/rosywrt/luci-theme-rosy package/luci-theme-rosy
