5611948b805c4b2c72b5626dae3e5e76d855564d
xujianjie1213 <35050408+xujianjie1213@users.noreply.github.com>
2020-04-21 20:18:05 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -12,3 +12,10 @@ git clone https://github.com/tty228/luci-app-serverchan package/luci-app-serverc
 git clone https://github.com/kenzok8/openwrt-packages package/openwrt-packages
 git clone https://github.com/kenzok8/small package/small
 git clone https://github.com/xujianjie1213/openwrt-smartdns package/openwrt-smartdns
+rm -rf package/lean/luci-theme-argon
+sed -i 's/LuCI Master/LuCI master by kenzo/g' /usr/lib/lua/luci/version.lua
+sed -i '/luciversion/d' /usr/lib/lua/luci/version.lua
+echo 'luciversion = "19.07"' >> /usr/lib/lua/luci/version.lua
+sed -i '/set luci.main.mediaurlbase=\/luci-static\/bootstrap/d' feeds/luci/themes/luci-theme-bootstrap/root/etc/uci-defaults/30_luci-theme-bootstrap
+./scripts/feeds update -a
+./scripts/feeds install
