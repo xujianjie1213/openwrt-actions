1be306bb20483d0082a856308894ffdcfa7eabf7
xujianjie1213 <35050408+xujianjie1213@users.noreply.github.com>
2020-07-03 14:28:34 +0800

    Update diy2.sh

--- diy2.sh
+++ diy2.sh
@@ -1,2 +1,23 @@
+#### Modify default IP
 sed -i 's/192.168.1.1/192.168.2.254/g' package/base-files/files/bin/config_generate
+
+#### add ext luci-app
+git clone https://github.com/kenzok8/openwrt-packages package/openwrt-packages
+git clone https://github.com/kenzok8/small package/small
+git clone https://github.com/tty228/luci-app-serverchan package/luci-app-serverchan
+
+#### add theme
+#git clone https://github.com/openwrt-develop/luci-theme-atmaterial package/luci-theme-atmaterial
+#git clone https://github.com/apollo-ng/luci-theme-darkmatter.git
+
+#### remove duplicate app-ssr-plus
+rm -rf package/openwrt-packages/luci-app-ssr-plus
+
+#### modify luc display 
 sed -i 's/LuCI Master/LuCI master by Rylan/g' /usr/lib/lua/luci/version.lua
+sed -i '/luciversion/d' /usr/lib/lua/luci/version.lua
+echo 'luciversion = "19.07"' >> /usr/lib/lua/luci/version.lua
+
+#### update and install feeds
+./scripts/feeds update -a
+./scripts/feeds install
