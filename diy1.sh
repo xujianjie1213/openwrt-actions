abb13c3a9fe2ba9b570bb05ea7a7d8bfe4a70d5c
xujianjie1213 <35050408+xujianjie1213@users.noreply.github.com>
2020-07-03 14:19:52 +0800

    Update diy1.sh

--- diy1.sh
+++ diy1.sh
@@ -6,8 +6,9 @@
 # Blog: https://p3terx.com
 #=================================================
 # Modify default IP
-#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
-git clone https://github.com/tty228/luci-app-serverchan package/luci-app-serverchan
-git clone https://github.com/kenzok8/openwrt-packages package/openwrt-packages
-git clone https://github.com/kenzok8/small package/small
-rm -rf package/openwrt-packages/luci-app-ssr-plus
+
+# Uncomment a feed source
+#sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+
+# Add a feed source
+#sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
