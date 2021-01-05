9715a55c2b35498a7e6c46525fe7def1da3bdb94
xujianjie1213 <35050408+xujianjie1213@users.noreply.github.com>
2021-01-05 09:49:17 +0800

    Update diy2.sh

--- diy2.sh
+++ diy2.sh
@@ -6,7 +6,7 @@
 # Blog: https://p3terx.com
 #=================================================
 #### Modify default IP
-sed -i 's/192.168.1.1/192.168.2.254/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
 
 #### add ext luci-app
 git clone https://github.com/kenzok8/openwrt-packages package/openwrt-packages
