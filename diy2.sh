19741b95d7102199c3a076547c793d3a180ae5a0
xujianjie1213 <35050408+xujianjie1213@users.noreply.github.com>
2021-02-26 14:45:36 +0800

    Update diy2.sh

--- diy2.sh
+++ diy2.sh
@@ -14,11 +14,13 @@ git clone https://github.com/kenzok8/small package/small
 git clone https://github.com/tty228/luci-app-serverchan package/luci-app-serverchan
 ### serverchan dingTalk
 git clone https://github.com/zzsj0928/luci-app-serverchand package/luci-app-serverchand
+#### add hello world
+git clone https://github.com/fw876/helloworld package/helloworld
 
 #### add theme
 
 #### remove duplicate app-ssr-plus
-#rm -rf package/openwrt-packages/luci-app-ssr-plus
+rm -rf package/openwrt-packages/luci-app-ssr-plus
 
 #### update and install feeds
 ./scripts/feeds update -a
