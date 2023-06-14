10f8f6ff911b468551415ae0d78f7ee2b4327f9e
xujianjie1213 <35050408+xujianjie1213@users.noreply.github.com>
2023-06-14 09:50:38 +0800

    Update diy2.sh

--- diy2.sh
+++ diy2.sh
@@ -10,7 +10,8 @@ sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generat
 
 #### add ext luci-app
 git clone https://github.com/kenzok8/small-package package/small-package
-
+rm -rf package/small-package/firewall
+rm -rf package/small-package/firewall4
 
 #### add theme
 
