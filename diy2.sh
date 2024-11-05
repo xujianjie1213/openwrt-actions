94bbb39159b5d06f1a184dbedcb607eed100b10f
xujianjie1213 <35050408+xujianjie1213@users.noreply.github.com>
2024-11-05 21:00:11 +0800

    Update diy2.sh

--- diy2.sh
+++ diy2.sh
@@ -8,13 +8,16 @@
 #### Modify default IP
 sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
 
-#### add ext luci-app
-#### git clone --depth=1 https://github.com/kenzok8/small-package package/small-package
-#### rm -rf package/small-package/firewall
-#### rm -rf package/small-package/firewall4
-
 #### add theme
+git clone --depth=1 https://github.com/kiddin9/luci-theme-edge package/luci-theme-edge
+git clone --depth=1 https://github.com/jerrykuku/luci-theme-argon package/luci-theme-argon
+git clone --depth=1 https://github.com/jerrykuku/luci-app-argon-config package/luci-app-argon-config
+git clone --depth=1 https://github.com/xiaoqingfengATGH/luci-theme-infinityfreedom package/luci-theme-infinityfreedom
 
+#### 修改版本为编译日期
+date_version=$(date +"%y.%m.%d")
+orig_version=$(cat "package/lean/default-settings/files/zzz-default-settings" | grep DISTRIB_REVISION= | awk -F "'" '{print $2}')
+sed -i "s/$orig_version/R$date_version/g" package/lean/default-settings/files/zzz-default-settings
 
 #### customize personal info
 echo '修改主机名称'
