a9b7a8bd0f8b73b004da706bada8a5fd64a5397e
xujianjie1213 <35050408+xujianjie1213@users.noreply.github.com>
2024-11-07 10:48:49 +0800

    Update diy2.sh

--- diy2.sh
+++ diy2.sh
@@ -9,10 +9,6 @@
 sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
 
 #### add theme
-git clone --depth=1 https://github.com/kiddin9/luci-theme-edge package/luci-theme-edge
-git clone --depth=1 https://github.com/jerrykuku/luci-theme-argon package/luci-theme-argon
-git clone --depth=1 https://github.com/jerrykuku/luci-app-argon-config package/luci-app-argon-config
-git clone --depth=1 https://github.com/xiaoqingfengATGH/luci-theme-infinityfreedom package/luci-theme-infinityfreedom
 
 #### 修改版本为编译日期
 date_version=$(date +"%y.%m.%d")
