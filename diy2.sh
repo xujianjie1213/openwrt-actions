8378a1c31f3c65c8d8a916e0564b8aca01d1f168
xujianjie1213 <35050408+xujianjie1213@users.noreply.github.com>
2024-11-21 21:08:30 +0800

    Update diy2.sh

--- diy2.sh
+++ diy2.sh
@@ -6,7 +6,8 @@
 # Blog: https://p3terx.com
 #=================================================
 #### Modify default IP
-sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate 
+echo "Change default LAN IP to 192.168.2.1"
 
 #### add theme
 
